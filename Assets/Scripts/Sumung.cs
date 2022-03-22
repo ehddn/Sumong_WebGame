a323d6d78ec6b3fcfa7159f3f36c1c1f65ed86c1
고동우 <ehddn2202@naver.com>
2022-03-23 06:43:56 +0900

    중간점검3

--- Assets/Scripts/Sumung.cs
+++ Assets/Scripts/Sumung.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Sumung : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class Sumung : MonoBehaviour
     private float g;
     private float b;
     public Image sumung;
+    public bool introType;
+    public bool turn;
+    private float time;
 
     // Start is called before the first frame update
     void Start()
@@ -19,20 +23,75 @@ public class Sumung : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        r = r + Time.deltaTime;
-        g = g + Time.deltaTime;
-        b = b + Time.deltaTime;
-        Appear();
+        time += Time.deltaTime;
+        if (turn == false)
+        {
+            Appear();
+        }
+        else
+        {
+            Disappear();
+        }
+        //Debug.Log(r);
+        
+        
 
     }
     public void Appear()
     {
-        if (r < 255)
+
+        
+        if (introType == true)
         {
-            sumung.color = new Color(r, g, b);
+            if (r < 2)
+            {
+                r = r + Time.deltaTime;
+                g = g + Time.deltaTime;
+                b = b + Time.deltaTime;
+                sumung.color = new Color(r, g, b);
+                if (r > 1.8)
+                {
+                    turn = true;
+                }
+
+            }
+            
+        }
+        else
+        {
+            if (r < 2)
+            {
+                r = r + Time.deltaTime;
+                g = g + Time.deltaTime;
+                b = b + Time.deltaTime;
+                sumung.color = new Color(r, g, b);
+
+            }
         }
 
         
+    }
+    public void Disappear()
+    {
+        
+        if (introType == true)
+        {
+            if (r < 2)
+            {
+                r = r - Time.deltaTime;
+                g = g - Time.deltaTime;
+                b = b - Time.deltaTime;
+                sumung.color = new Color(r , g , b );
+            }
+
+            if (time > 3.5)
+            {
+                SceneManager.LoadScene("Start");
+            }
+
+        }
+     
+
     }
     /*IEnumerator delay()
     {
