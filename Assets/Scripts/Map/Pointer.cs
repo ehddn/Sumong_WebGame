07dbfa31fc95a73e94617f63990111369bd1dd52
smatphone <jb5982@naver.com>
2022-03-25 11:50:09 +0900

    오류 수정(내용 참고)
    1. 시작화면 노래 루프
    2. 맵 이동 뚫고 가는 현상 수정
    3. 똥피하기 버튼 깨지는 현상 수정
    4. 퀴즈에서 텍스트 바 크기 조절(정답/오답 창 밖으로 튀어나옴)
    5. 맵에서 홈페이지 이동 버튼 위치 조정

--- Assets/Scripts/Map/Pointer.cs
+++ Assets/Scripts/Map/Pointer.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Pointer : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -19,14 +19,16 @@ public class Pointer : MonoBehaviour
     public IEnumerator move(List<GameObject> wayPoint)
     {
         int i = 0;
+        float degree = 100;
 
         while(i < wayPoint.Count - 1)
         {
             Vector2 now = new Vector2(transform.position.x, transform.position.y);
             Vector2 target = new Vector2(wayPoint[i + 1].transform.position.x, wayPoint[i + 1].transform.position.y);
 
-            if ((now - target).magnitude < 0.01f)
+            if (degree < (now - target).magnitude)
             {
+                degree = 100;
                 i++;
             }
             else
@@ -35,6 +37,8 @@ public class Pointer : MonoBehaviour
                 dir = dir.normalized;
                 transform.position = transform.position + dir * Time.deltaTime;
                 transform.position = new Vector3(transform.position.x, transform.position.y, -5);
+
+                degree = (now - target).magnitude;
             }
             yield return null;
         }
